3c7258d1bdce93efc82759845e4abe110f3f4aba
mkwiseman <163409897+mkwiseman@users.noreply.github.com>
2024-03-28 15:02:32 +0000

    login_out

--- pos/accounts/urls.py
+++ pos/accounts/urls.py
@@ -3,4 +3,5 @@ from .views import login_view
 
 urlpatterns = [
     path('login/', login_view, name='login'),
+    path('logout/', views.user_logout, name='logout'),
 ]
--- pos/accounts/views.py
+++ pos/accounts/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+# accounts/views.py
+from django.contrib.auth import authenticate, login, logout
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import AuthenticationForm
 
-# Create your views here.
+def user_login(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(request, request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                login(request, user)
+                return redirect('home')  # Redirect to home page after login
+    else:
+        form = AuthenticationForm()
+    return render(request, 'accounts/login.html', {'form': form})
+
+def user_logout(request):
+    logout(request)
+    return redirect('login')  # Redirect to login page after logout
